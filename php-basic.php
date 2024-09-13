c46c0834f637571c10fe2a101bc2a4f28e1977a9
unknown <123@gmail.com>
2024-09-13 10:56:00 +0300

    php cli

---
+++ php-basic.php
@@ -0,0 +1,15 @@
+<?php
+ echo "this is my first cli";
+ for ($x = 1; $x <= 10; $x++) {
+    echo "The number is: $x <br>";
+  }
+  for ($y = 10; $y >= 1; $y--) {
+    echo "The number is: $y <br>";
+  }
+  $peoples = [
+    'person1' => ['name'=> 'Janis', 'age'=> 18],
+    'person2' => ['name'=> 'Juris', 'age'=> 20],
+    'person3' => ['name'=> 'antra', 'age'=> 24],]
+  
+  
+  
---
+++ to-do-list.php
@@ -0,0 +1,29 @@
+<?php
+//$name = readline("ievadi savu vārdu:\n");
+//echo "Your name is $name";
+
+
+do {
+    echo "To do list\n";
+    echo "ievadīt jaunu uzdevumu => 1\n"
+    $choice = readline("IZVELIES DARBIBU: ")
+   
+
+switch ($choice) {
+  case '1':
+    echo "to be implimented\n";
+    break;
+  case '1':
+    echo "to be implimented\n";
+    break;
+  case '3':
+    echo "to be implimented\n";
+    break;
+  default:
+    echo "Invalid option!\n";
+}
+
+   $continue = readline("Vai vēlies turpinā\n")
+    $x++;
+  } while ($continue != "N");
+  
